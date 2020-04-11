a04f085c96f471b9d59d1965168e2797f702646e
Bryan Lopez <lopez95.bryan@gmail.com>
2020-04-11 17:28:37 -0500

    Fix - Add dinamic date for load data csv from github

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/coronamon/server"
 )
 
+//https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series --> Global by contry
+//https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports --> Global by last day/country
+
 func main() {
 	http.HandleFunc("/", server.Server)
 	http.ListenAndServe(":3000", nil)

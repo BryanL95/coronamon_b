8d1df89ee9f2963f0a47508ffa43705aeb1d4084
Bryan Lopez <lopez95.bryan@gmail.com>
2020-04-18 15:29:40 -0500

    Fix - Change the step for add to generaljson and validate into the same generaljson

--- data/generalData.go
+++ data/generalData.go
@@ -3,7 +3,6 @@ package data
 import (
 	"encoding/csv"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -52,42 +51,41 @@ func requestConfirmed() error {
 		return err
 	}
 
-	temporal := make(map[string]int) //We can't access to memory address, because map is dinamic, so declare into function
-
 	for id, row := range dataReader {
 		if id == 0 {
 			continue
 		}
 		counter, _ := strconv.Atoi(row[len(row)-1])
-		if _, ok := temporal[row[1]]; ok {
-			temporal[row[1]] += counter
-		} else {
-			temporal[row[1]] = counter
-		}
-	}
-
-	if error := temporalToStruct(temporal); error != nil {
-		log.Println(error)
-		return error
+		temporalToStruct(row[1], counter, &globalJson)
 	}
 
 	return nil
 }
 
-func temporalToStruct(data map[string]int) error {
-	if len(data) > 0 {
-		for key, val := range data {
-			var localData GeneralJSON
-			localData.Country = key
-			localData.Confirmed = int32(val)
-
-			globalJson = append(globalJson, localData)
+func temporalToStruct(country string, number int, allData *[]GeneralJSON) {
+	var localData GeneralJSON
+	if len(*allData) > 0 {
+		if ok := validateExist(country, allData); ok != -1 {
+			(*allData)[ok].Confirmed += int32(number)
+		} else {
+			localData.Country = country
+			localData.Confirmed = int32(number)
+			*allData = append(*allData, localData)
 		}
 	} else {
-		return errors.New("Size data from global confirmed is empty")
+		localData.Country = country
+		localData.Confirmed = int32(number)
+		*allData = append(*allData, localData)
 	}
+}
 
-	return nil
+func validateExist(country string, allData *[]GeneralJSON) int {
+	for key, val := range *allData {
+		if val.Country == country {
+			return key
+		}
+	}
+	return -1
 }
 
 /*func requestDeaths() {
